d6750cbeada4c1cc1f19e4d6e530376d6f05fcab
absyinka <absyinka@gmail.com>
2023-11-27 13:59:49 +0100

    Initial commit

---
+++ fire.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCkUwTwtzLl_x0VKGrNhGv2EqqC4ef-E2s',
+  authDomain: 'jalsasalanaregistrationstats.firebaseapp.com',
+  projectId: 'jalsasalanaregistrationstats',
+  storageBucket: 'jalsasalanaregistrationstats.appspot.com',
+  messagingSenderId: '976347870835',
+  appId: '1:976347870835:web:6e283a231bf6b6916c1c06',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
